0881ff438c0aa253fa54a72646c270a6a8430d98
ROMAN BEZNOSIUK <89782854+ROMAN1411@users.noreply.github.com>
2022-02-11 02:15:10 +0200

    Сompleted 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let timerId = null;
+stopBtn.setAttribute('disabled', true);
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+    startBtn.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled', true);
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled', true);
+    stopBtn.setAttribute('disabled', true);
+    });
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
