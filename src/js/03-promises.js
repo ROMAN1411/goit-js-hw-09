ed2526eb43e5afa0025430f16877c35afce21793
ROMAN BEZNOSIUK <89782854+ROMAN1411@users.noreply.github.com>
2022-02-15 19:37:13 +0200

    Сompleted 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+Notiflix.Notify.init({
+    position: 'center-top',
+    distance: '50px',
+});
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  let delay = Number(evt.currentTarget.delay.value);
+  let step = Number(evt.currentTarget.step.value);
+  let amount = Number(evt.currentTarget.amount.value);
+  
+  for (let position = 1; position <= amount; position++) {
+    delay += step;
+
+  createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}&#160;ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}&#160;ms`);
+  });
   }
+}
+
+function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (shouldResolve) {
+                resolve({ position, delay });
+            } else {
+                reject({ position, delay });
+            }
+        }, delay);
+    });
 }
